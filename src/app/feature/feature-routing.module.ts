d62c644c44a4f82a27bb7e07aa8f2a0c4f9450dc
maen <maen210212@gmail.com>
2023-04-22 13:01:32 -0600

    Configure basic routing and folder's structure

---
+++ src/app/feature/feature-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: "authentication",
+    loadChildren: () => import("./auth/auth.module").then(a => a.AuthModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class FeatureRoutingModule { }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+/**
+Dentro de esta archivo se crea la ruta al api
+Crear rutas de ambiente
+**/
+production: false
+apiUrl: "http://localhost:8080/api/"
+};
